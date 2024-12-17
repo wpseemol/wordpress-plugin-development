b7aa05f3d54fd9735b152bfcffbf0de76a200ce8
wpseemol <wpseemol@gmail.com>
2024-12-17 22:19:15 +0600

    custom filde image set customly

--- includes/Custom_Columns.php
+++ includes/Custom_Columns.php
@@ -23,21 +23,63 @@ class Custom_Column
 
     public function manage_page_post_columns_callback($columns)
     {
-        $columns["id"] = "ID";
 
-        return $columns;
+        $new_columns = array();
+
+
+        foreach ($columns as $key => $column) {
+
+
+            if ("title" === $key) {
+                $new_columns["id"] = "ID";
+                $new_columns["images"] = "Images";
+            }
+
+            $new_columns[$key] = $column;
+        }
+
+
+
+
+        return $new_columns;
     }
 
 
     public function manage_page_posts_custom_column_callback($column_name, $post_id)
     {
-        echo $post_id;
+
+        if ("images" === $column_name) {
+
+            $url = get_the_post_thumbnail_url($post_id, "thumbnail");
+
+            $title = get_the_title($post_id);
+
+            if ($url) {
+                echo "<img src='$url' alt='$title' />";
+            }
+
+            echo "<p>$title</p>";
+
+
+
+            return;
+        }
+
+
+        if ("id" === $column_name) {
+            echo $post_id;
+            return;
+        }
+
+
     }
 
     public function manage_page_sortable_columns_callback($columns)
     {
 
         $columns["id"] = "id";
+
+
 
         return $columns;
     }
